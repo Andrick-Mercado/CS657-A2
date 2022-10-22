c4958ca93f2b9a6f6324032a7391e299d3ead5df
Andrick <andrickmercado17@gmail.com>
2022-10-22 10:48:37 -0700

    Implementation for one warehouse
    Implementation for one warehouse

--- CS657-A2-Unity/Assets/Scripts/Robot/StudentSolution.cs
+++ CS657-A2-Unity/Assets/Scripts/Robot/StudentSolution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using GeneticAlgorithm;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,7 @@ public class StudentSolution : MonoBehaviour
     public int[,] ObstacleMap;
     public Dropdown dropdownmoves;
     public int direction;
+    public LineRenderer lineRenderer;
 
     [HideInInspector] public bool SolutionFound;
     [HideInInspector] public float envirement;
@@ -33,14 +35,27 @@ public class StudentSolution : MonoBehaviour
         envirement = 0.9f;
         direction = 0;
         maxGenerations = 600;
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     public void GenerateValuesArray()
     {
+        // change the house creation outside of class so we can give it as an input to each warehouse
+        // make warehouse location a given variable to create the class
+        // compare best fitness to each warehouse and remove values accordingly (maybe: make to list )
         
-        Population test = new Population(0.7f, 0.1f, 10,10, maxGenerations);
+        Population test = new Population(0.7f, 0.5f, 20, 10000);
         ObstacleMap = test.CityAreaGrid;
 
+        Vector2[] otherTest = test.GetPopulation();
+        lineRenderer.positionCount = otherTest.Length+2;
+        lineRenderer.SetPosition(0, new Vector3(10f,5f, 0f));
+        for (int i = 1; i <= lineRenderer.positionCount-2; i++)
+        {
+            lineRenderer.SetPosition(i, new Vector3(otherTest[i-1].x,otherTest[i-1].y, 0f));
+        }
+        lineRenderer.SetPosition(lineRenderer.positionCount-1, new Vector3(10f,5f, 0f));
+            
         GameManager.Instance.ChangeState(GameState.GenerateGrid);
     }
 
